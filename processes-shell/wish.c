8d00276b7a6d0d40488778a94a7289e23fde680c
Tim Bedford <tmbdfd@gmail.com>
2020-06-14 18:50:58 -0400

    Add correct while loop structure to program
    The previous iteration ran one argument-less command as an argument
    from the command line, which is not what the assignment required. This
    basic functionality has now been reproduced through a while loop as
    the interactive mode.
    
    Specific errors have been removed and replaced with a single generic
    error wrapped in a function.

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -4,33 +4,60 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(int argc, char *argv[])
+void printError()
+{
+  char error_message[30] = "An error has occurred\n";
+  write(STDERR_FILENO, error_message, strlen(error_message));
+  exit(1);
+}
+
+void runCommand(char *path, char *line)
+{
+  int canAccess = access(path, X_OK);
+  if (canAccess == 0) {
+    int rc = fork();
+    if (rc < 0) {
+      printError();
+    } else if (rc == 0) {
+      char cmd[] = "";
+      strcpy(cmd, path);
+      strcat(cmd, "/");
+      strcat(cmd, line);
+      char *myargs[] = {cmd, NULL};
+      execv(myargs[0], myargs);
+      // Anything past the execv command will be reached only if there was an error
+      printError();
+    } else {
+      int rc_wait = wait(NULL);
+    }
+  } else {
+    printError();
+  }
+}
+
+void runInteractiveMode()
 {
   char *path = "/bin";
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  printf("wish> ");
+  while ((linelen = getline(&line, &linecap, stdin)) > 0) {
+    runCommand(path, strsep(&line, "\n"));
+    printf("wish> ");
+  }
+}
+
+int main(int argc, char *argv[])
+{
   if (argc == 2) {
-    int canAccess = access(path, X_OK);
-    if (canAccess == 0) {
-      int rc = fork();
-      if (rc < 0) {
-	fprintf(stderr, "Fork failed\n");
-        exit(1);
-      } else if (rc == 0) {
-	char cmd[] = "";
-	strcpy(cmd, path);
-	strcat(cmd, "/");
-	strcat(cmd, argv[1]);
-	char *myargs[] = {cmd, NULL};
-	execv(myargs[0], myargs);
-      } else {
-	int rc_wait = wait(NULL);
-      }
-    } else {
-      fprintf(stderr, "Cannot access path\n");
-    }
-  } else if (argc > 2) {
-     fprintf(stderr, "Please enter a command with no arguments\n");
+    // run batch file
+    printf("Batch file option not yet implemented\n");
+  } else if (argc == 1) {
+    // enter interactive mode
+    runInteractiveMode();
   } else {
-     fprintf(stderr, "No command entered\n");
+    printError();
   }
   return 0;
 }
