55e04e793c4552b218b4d2b79e76533ddccd89de
Tim Bedford <tmbdfd@gmail.com>
2020-06-22 13:37:41 -0400

    Refactor redirection logic
    In order to better handle each piece of a command--especially in
    anticipation of implementing parallel commands--commands are now put
    into a struct. There structs are created before checking the built-in
    commands and elimiate the clumsy mess of endless else/if blocks that
    followed the built-ins. It's still a bit clumsy, but more siginicantly
    more legible to me.
    
    The issue mentioned in the last commit was simply because of a mistake
    in the trimWhiteSpace function.

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -7,11 +7,34 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <stdbool.h>
+
+typedef struct {
+  char *program_w_args[100];
+  int output;
+} command;
 
 void printError()
 {
   char error_message[30] = "An error has occurred\n";
   write(STDERR_FILENO, error_message, strlen(error_message));
+}
+
+void printPrompt(FILE *fpinput)
+{
+  if (fpinput == stdin) {
+    printf("wish> ");
+  }
+}
+
+bool isThereWhiteSpace(char *list)
+{
+  for (int i = 0; i < strlen(list); i++) {
+    if (isspace(list[i])) {
+      return true;
+    }
+  }
+  return false;
 }
 
 // Function found here: https://stackoverflow.com/a/122721
@@ -35,12 +58,15 @@ char *trimWhiteSpace(char *str)
     end--;
   }
 
+  // Write new null terminator character
+  end[1] = '\0';
+
   return str;
 }
 
-int runCommand(char *path, char **command_w_args, int output)
+int runCommand(char *path, command command)
 {
-  char command_w_path[100] = "";
+  char program_w_path[100] = "";
   int can_access = access(path, X_OK);
   int status;
   int save_out;
@@ -54,23 +80,22 @@ int runCommand(char *path, char **command_w_args, int output)
       // Redirection logic cribbed from here: https://stackoverflow.com/a/8517401
       save_out = dup(fileno(stdout));
       save_err = dup(fileno(stderr));
-      if (-1 == dup2(output, fileno(stdout))) {
+      if (-1 == dup2(command.output, fileno(stdout))) {
 	return errno;
       }
-      if (-1 == dup2(output, fileno(stderr))) {
+      if (-1 == dup2(command.output, fileno(stderr))) {
 	return errno;
       }
-
-      strcpy(command_w_path, path);
-      strcat(command_w_path, "/");
-      strcat(command_w_path, command_w_args[0]);
-      command_w_args[0] = command_w_path;
-      execv(command_w_args[0], command_w_args);
+      strcpy(program_w_path, path);
+      strcat(program_w_path, "/");
+      strcat(program_w_path, command.program_w_args[0]);
+      command.program_w_args[0] = program_w_path;
+      execv(command.program_w_args[0], command.program_w_args);
 
       // Anything past the execv command will be reached only if there was an error
       fflush(stdout);
       fflush(stderr);
-      close(output);
+      close(command.output);
       dup2(save_out, fileno(stdout));
       dup2(save_err, fileno(stderr));
       close(save_out);
@@ -88,10 +113,10 @@ int runCommand(char *path, char **command_w_args, int output)
   return 0;
 }
 
-void runThroughEachPath(char *command_w_args[100], char *paths[], int paths_len, int output)
+void runThroughEachPath(command command, char *paths[], int paths_len)
 {
   for (int i = 0; i < paths_len; i++) {
-    int is_command_successful = runCommand(paths[i], command_w_args, output);
+    int is_command_successful = runCommand(paths[i], command);
     if (is_command_successful == 0) {
       // No more paths need to be tested
       break;
@@ -113,9 +138,7 @@ void runCommandLoop(FILE *fpinput)
   int command_index = 0;
   int paths_len = 1;
 
-  if (fpinput == stdin) {
-    printf("wish> ");
-  }
+  printPrompt(fpinput);
 
   while ((linelen = getline(&line, &linecap, fpinput)) > 0) {
     if (strcmp(line, "\n") == 0) {
@@ -124,40 +147,90 @@ void runCommandLoop(FILE *fpinput)
       line = strsep(&line, "\n");
     }
 
+    command command;
+    int count = 0;
+    char *separated_line[100];
+
+    while ((separated_line[count] = strsep(&line, ">")) != NULL) {
+      count += 1;
+    }
+
+    if (count > 2) {
+      // There are too many redirection commands
+      printError();
+      printPrompt(fpinput);
+      continue;
+    } else if (count == 2) {
+      if (strcmp(separated_line[0], "") == 0) {
+	// There's nothing before the redirection command
+	printError();
+	printPrompt(fpinput);
+	continue;
+      } else if (strcmp(separated_line[1], "") == 0) {
+	// There's nothing after the redirection command
+	printError();
+	printPrompt(fpinput);
+	continue;
+      } else {
+	separated_line[1] = trimWhiteSpace(separated_line[1]);
+	// There's more than one file being passed to the redirection command
+	if (isThereWhiteSpace(separated_line[1])) {
+	  printError();
+	  printPrompt(fpinput);
+	  continue;
+	}
+	// Open what's after the redirection command as a file
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	int output = open(trimWhiteSpace(separated_line[1]),
+			  O_RDWR | O_CREAT | O_TRUNC, mode);
+	if (output == -1) {
+	  printError();
+	  printPrompt(fpinput);
+	  continue;
+	} else {
+	  command.output = output;
+	}
+      }
+    } else {
+      // There is no redirection command
+      command.output = fileno(stdout);
+    }
+
+    separated_line[0] = trimWhiteSpace(separated_line[0]);
     // Method found here: http://c-for-dummies.com/blog/wp-content/uploads/2016/02/0220b.c
-    while ((command_w_args[command_index] = strsep(&line, " ")) != NULL) {
+    while ((command.program_w_args[command_index] = strsep(&separated_line[0], " ")) != NULL) {
       command_index += 1;
     }
 
     // Remove white space from each string in command_w_args
-    for (int i = 0; i < command_index-1; i++) {
-      command_w_args[i] = trimWhiteSpace(command_w_args[i]);
+    for (int i = 0; i < command_index; i++) {
+      command.program_w_args[i] = trimWhiteSpace(command.program_w_args[i]);
     }
 
-    if (strcmp(command_w_args[0], "exit") == 0) {
+    if (strcmp(command.program_w_args[0], "exit") == 0) {
       if (command_index == 1) {
 	// exit is only valid with no arguments
 	exit(0);
       } else {
 	printError();
       }
-    } else if (strcmp(command_w_args[0], "cd") == 0) {
+    } else if (strcmp(command.program_w_args[0], "cd") == 0) {
       if (command_index == 2) {
 	// cd is only valid with one argument
-	if ((chdir(command_w_args[1])) == -1) {
+	if ((chdir(command.program_w_args[1])) == -1) {
 	  printError();
 	}
       } else {
 	printError();
       }
-    } else if (strcmp(command_w_args[0], "path") == 0) {
+    } else if (strcmp(command.program_w_args[0], "path") == 0) {
       if (command_index == 1) {
 	// Erase all paths
 	memset(&paths[0], 0, sizeof(paths));
 	paths_len = 0;
       } else {
 	for (int i = 0; i < command_index-1; i++) {
-	  paths[i] = command_w_args[i+1];
+	  paths[i] = command.program_w_args[i+1];
 	}
 	paths_len = command_index - 1;
       }
@@ -167,40 +240,7 @@ void runCommandLoop(FILE *fpinput)
 	// There are no paths, so only built-in commands could have worked
 	printError();
       } else {
-	// Check for redirection commands
-	int redirection_command_list[command_index];
-	int rdc_index = 0;
-	for (int i = 0; i < command_index; i++) {
-	  if (strcmp(command_w_args[i], ">") == 0) {
-	    redirection_command_list[rdc_index] = i;
-	    rdc_index += 1;
-	  }
-	}
-	if ((rdc_index > 0) && (redirection_command_list[0] != command_index-2)) {
-	  // A redirection command exists in a wrong location:
-	  // 1. If in the last position, there is no file
-	  // 2. If in the third to last position or earlier, there are too many files
-	  printError();
-	} else if (rdc_index > 1) {
-	  // There is more than one redirection command, which is not allowed
-	  printError();
-	} else {
-	  if (rdc_index == 0) {
-	    // There are no redirection commands
-	    runThroughEachPath(command_w_args, paths, paths_len, fileno(stdout));
-	  } else {
-	    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-	    int output = open(command_w_args[command_index-1], O_RDWR | O_CREAT | O_TRUNC, mode);
-	    if (output == -1) {
-	      printError();
-	    } else {
-	      // Take away the redirection command and file name
-	      command_w_args[command_index-1] = NULL;
-	      command_w_args[command_index-2] = NULL;
-	      runThroughEachPath(command_w_args, paths, paths_len, output);
-	    }
-	  }
-	}
+	runThroughEachPath(command, paths, paths_len);
       }
     }
 
@@ -208,9 +248,7 @@ void runCommandLoop(FILE *fpinput)
     memset(&command_w_args[0], 0, sizeof(command_w_args));
     command_index = 0;
 
-    if (fpinput == stdin) {
-      printf("wish> ");
-    }
+    printPrompt(fpinput);
   }
 }
 
